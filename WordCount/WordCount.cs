ab9042a6d101bb9b4ca83b8cc1a4c5a322cf7a57
Elton <st10426@hotmail.com>
2017-08-30 23:58:06 +0800

    add first testcase and failed

---
+++ WordCount/WordCount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordCount
+{
+    public class WordCount
+    {
+        public static string CountWord(string input)
+        {
+            throw new ArgumentException();
+        }
+    }
+}
---
+++ WordCount/WordCountTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace WordCount
+{
+    public class WordCountTest
+    {
+        [Test]
+        public void NoWordGiven()
+        {
+            String input = null;
+            String expected = "";
+
+            Assert.AreEqual(expected, WordCount.CountWord(input));
+        }
+    }
+}
