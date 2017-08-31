b3bcb49052cf97dd1615b747903c6cb3953e1e75
Elton <st10426@hotmail.com>
2017-08-31 20:58:06 +0800

    add first teastcase and failed

---
+++ StringAverage/StringAverageTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace StringAverage
+{
+    public class StringAverageTest
+    {
+        [Test]
+        public void NoNumberGiven()
+        {
+            String input = null;
+            String expected = "";
+
+            Assert.AreEqual(expected, StringAverage.Average(input));
+        }
+    }
+}
---
+++ StringAverage/Stringaverage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAverage
+{
+    public class StringAverage
+    {
+        public static string Average(string input)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
