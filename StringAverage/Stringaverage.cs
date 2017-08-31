ca01a2c168bc8400ae61a9f27987bb1e1bd3e998
Elton <st10426@hotmail.com>
2017-08-31 21:40:46 +0800

    pass the test and refactoring

--- StringAverage/Stringaverage.cs
+++ StringAverage/Stringaverage.cs
@@ -8,24 +8,20 @@ namespace StringAverage
 {
     public class StringAverage
     {
+        private static Dictionary<string, int> numberDic = new Dictionary<string, int>()
+        {
+            {"zero", 0},
+            {"one", 1},
+            {"two", 2}
+        };
+
         public static string Average(string input)
         {
             if (string.IsNullOrEmpty(input))
                 return "n/a";
 
-            var numberDic = new Dictionary<string, int>()
-            {
-                {"one", 1},
-                {"two", 2}
-            };
-
             var numberList = input.Split(' ');
-            int sum = 0;
-            foreach (var number in numberList)
-            {
-                sum += numberDic[number];
-            }
-
+            int sum = numberList.Select(x => numberDic[x]).Sum();
             return numberDic.FirstOrDefault(x=>x.Value == sum / numberList.Length).Key;
         }
     }
